c817b158370f7284124e6e0aa4524b5279c0c115
psolmor <pedro.solermor@gmail.com>
2025-03-05 18:12:49 +0100

    Add progress bar

--- src/main.cc
+++ src/main.cc
@@ -12,6 +12,7 @@ int main(){
     cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for(int j=0;j<image_height;j++){
+        clog << "\rScanlines remaining: " << (image_height-j) << ' ' << flush;
         for(int i=0;i<image_width;i++){
             auto r=double(i)/(image_width-1);
             auto g=double(j)/(image_height-1);
@@ -24,4 +25,5 @@ int main(){
             cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+    clog << "\rDone.        \n";
 }
